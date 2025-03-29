8e429ace1faeea1d6cb120847b14648ba84e76b1
eric-zaharia <zahariaeric@gmail.com>
2025-03-29 17:55:19 +0200

    made send mail method async

--- backend/src/main/java/com/licenta/backend/service/MailService.java
+++ backend/src/main/java/com/licenta/backend/service/MailService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,7 @@ public class MailService {
     @Value("${spring.mail.username}")
     private String sender;
 
+    @Async
     public void sendSimpleMail(MailDetails mailDetails) {
 
         SimpleMailMessage mailMessage
